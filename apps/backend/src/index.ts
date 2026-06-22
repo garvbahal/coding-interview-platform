fae1c63a1afabbf334b63630fba447b9176ca15a
garvbahal <garvbahal75@gmail.com>
2026-06-22 11:57:17 +0530

    db_migration

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,2 +1,16 @@
 import express from "express";
 const app = express();
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+httpServer.listen(3000, () => {
+  console.log("HTTP Server is started at 3000 port");
+});
